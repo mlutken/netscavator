4f42d80d36c099da0e4860654e3b985db7da0252
Martin Lütken <martin@lutken.dk>
2023-02-09 14:31:19 +0100

    snapcraft-stage.sh updated

--- snapcraft-stage.sh
+++ snapcraft-stage.sh
@@ -3,27 +3,45 @@
 #--- Config vars ---
 PHP_VER="5.6.40"
 
-CRAWLER_REPOSITORY_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PARENT_NETSCAVATOR_DIR="$( dirname "${CRAWLER_REPOSITORY_ROOT_DIR}" )"
+REPOSITORY_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENT_NETSCAVATOR_DIR="$( dirname "${REPOSITORY_ROOT_DIR}" )"
 CODE_BUILD_ROOT_DIR="${PARENT_NETSCAVATOR_DIR}/_build"
-CRAWLER_BUILD_ROOT_DIR="${CODE_BUILD_ROOT_DIR}/crawler"
+NETSCAVATOR_BUILD_ROOT_DIR="${CODE_BUILD_ROOT_DIR}/netscavator"
 
-PROJECT_CODE_DIR=${CRAWLER_REPOSITORY_ROOT_DIR}/code
+PROJECT_CODE_DIR=${REPOSITORY_ROOT_DIR}/code
 PROJECT_CPP_DIR=${PROJECT_CODE_DIR}/cpp
 
 ### . ${PROJECT_CODE_DIR}/3rdpartysrc/webkit_version.env
 
 # --- Make sure code is built ---
-## bash ${CRAWLER_REPOSITORY_ROOT_DIR}/build.sh -b=release
+## bash ${REPOSITORY_ROOT_DIR}/build.sh -b=release
 
+# ---------------------
+# --- Detect system ---
+# ---------------------
+BITWIDTH=64
+
+DISTRIB=linux
+
+if type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    DISTRIB_ID=$(lsb_release -si)
+    DISTRIB_RELEASE=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release # Get system info
+fi
+
+# --- Lowercase DISTRIB_ID ---
+DISTRIB_ID=${DISTRIB_ID,,}
+DISTRIB=${DISTRIB_ID}${DISTRIB_RELEASE}
 
 # -------------------
 # --- Netscavator ---
 # -------------------
 
-SRC_DIR_MANUAL=${CRAWLER_BUILD_ROOT_DIR}/NetscavatorManual
-SRC_DIR_EXE=${CRAWLER_BUILD_ROOT_DIR}/bin
-SRC_DIR_LIBS=${CRAWLER_BUILD_ROOT_DIR}/bin
+SRC_DIR_MANUAL=${REPOSITORY_ROOT_DIR}/doc
+SRC_DIR_EXE=${NETSCAVATOR_BUILD_ROOT_DIR}/bin
+SRC_DIR_LIBS=${NETSCAVATOR_BUILD_ROOT_DIR}/bin
 SRC_DIR_PHP=${PROJECT_CODE_DIR}/3rdparty/php-${PHP_VER}/linux_${BITWIDTH}_release/lib
 SRC_DIR_QT=${PROJECT_CODE_DIR}/3rdparty/${WEBKIT_DIR}/linux_${BITWIDTH}_release
 SRC_DIR_SCRIPTCRAWL=${PROJECT_CPP_DIR}/scriptcrawl
@@ -31,18 +49,22 @@ SRC_DATA_DIR=${PROJECT_CPP_DIR}/data
 
 DST_BIN_DIR=/root/stage/bin
 
-echo "CRAWLER_BUILD_ROOT_DIR: ${CRAWLER_BUILD_ROOT_DIR}"
-echo "PROJECT_CODE_DIR      : ${PROJECT_CODE_DIR}"
-echo "PROJECT_CPP_DIR       : ${PROJECT_CPP_DIR}"
-echo "DISTRIB               : ${DISTRIB}"
+echo "DISTRIB_ID                : ${DISTRIB_ID}"
+echo "DISTRIB_RELEASE           : ${DISTRIB_RELEASE}"
+echo "DISTRIB                   : ${DISTRIB}"
+echo "REPOSITORY_ROOT_DIR       : ${REPOSITORY_ROOT_DIR}"
+echo "NETSCAVATOR_BUILD_ROOT_DIR: ${NETSCAVATOR_BUILD_ROOT_DIR}"
+echo "PROJECT_CODE_DIR          : ${PROJECT_CODE_DIR}"
+echo "PROJECT_CPP_DIR           : ${PROJECT_CPP_DIR}"
+echo "DISTRIB                   : ${DISTRIB}"
 
-echo "SRC_DIR_EXE           : ${SRC_DIR_EXE}"
-echo "SRC_DIR_LIBS          : ${SRC_DIR_LIBS}"
-echo "SRC_DATA_DIR          : ${SRC_DATA_DIR}"
-echo "SRC_DIR_PHP           : ${SRC_DIR_PHP}"
+echo "SRC_DIR_EXE               : ${SRC_DIR_EXE}"
+echo "SRC_DIR_LIBS              : ${SRC_DIR_LIBS}"
+echo "SRC_DATA_DIR              : ${SRC_DATA_DIR}"
+echo "SRC_DIR_PHP               : ${SRC_DIR_PHP}"
 echo " "
-echo "DST_BIN_DIR           : ${DST_BIN_DIR}"
-echo "SRC_DIR_QT            : ${SRC_DIR_QT}"
+echo "DST_BIN_DIR               : ${DST_BIN_DIR}"
+echo "SRC_DIR_QT                : ${SRC_DIR_QT}"
 
 # exit 1; # FIXMENM
 
@@ -52,11 +74,12 @@ mkdir -p ${DST_BIN_DIR}/robot
 mkdir -p ${DST_BIN_DIR}/data
 mkdir -p ${DST_BIN_DIR}/data/php/miner_utils
 mkdir -p ${DST_BIN_DIR}/data/php/miner_templates
+mkdir -p /usr/share/netscavator/doc
 
 
 echo Copy Netscavator files to staging ...
 
-cp -r ${SRC_DIR_MANUAL} ${DST_BIN_DIR}
+cp -r ${REPOSITORY_ROOT_DIR}/doc /usr/share/netscavator/doc
 cp ${SRC_DATA_DIR}/bin/linux/install-xvfb.sh ${DST_BIN_DIR}
 cp -r ${SRC_DATA_DIR}/robot/* ${DST_BIN_DIR}/robot/
 cp ${SRC_DATA_DIR}/config/php.ini.in ${DST_BIN_DIR}
