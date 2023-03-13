b6a95acc86b1c40b0cc6cbb4609a411ad8e788d2
Martin Lütken <martin@lutken.dk>
2023-03-13 18:47:51 +0100

    Fix appimage-build.sh

--- netcreator-appimage-build.sh
+++ netcreator-appimage-build.sh
@@ -22,18 +22,13 @@ BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" appimage-build-XXXXXX)
 
 
 # make sure to clean up build dir, even if errors occur
-
 cleanup () {
-
     if [ -d "$BUILD_DIR" ]; then
-
         rm -rf "$BUILD_DIR"
-
     fi
-
 }
 
-trap cleanup EXIT
+# trap cleanup EXIT
 
 
 # store repo root as variable
@@ -42,6 +37,8 @@ REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
 
 OLD_CWD=$(readlink -f .)
 
+echo "OLD_CWD: '$OLD_CWD'"
+
 
 # switch to build dir
 
@@ -49,16 +46,10 @@ pushd "$BUILD_DIR"
 
 
 # configure build files with CMake
-
 # we need to explicitly set the install prefix, as CMake's default is /usr/local for some reason...
-
-cmake "$REPO_ROOT" -DCMAKE_INSTALL_PREFIX=/usr
-
-
 # build project and install files into AppDir
-
+cmake "$REPO_ROOT" -DCMAKE_INSTALL_PREFIX=/usr
 make -j$(nproc)
-
 make install DESTDIR=AppDir
 
 cp /usr/lib/x86_64-linux-gnu/nss/* AppDir/usr/lib
@@ -70,27 +61,20 @@ cp ./bin/qtwebengine_resources.pak AppDir/usr/bin/
 # now, build AppImage using linuxdeploy and linuxdeploy-plugin-qt
 
 # download linuxdeploy and its Qt plugin
-
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-
 wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 
-
 # make them executable
-
 chmod +x linuxdeploy*.AppImage
 
 
 # make sure Qt plugin finds QML sources so it can deploy the imported files
-
-export QML_SOURCES_PATHS="$REPO_ROOT"/src
+# export QML_SOURCES_PATHS="$REPO_ROOT"/src
 
 
 # initialize AppDir, bundle shared libraries for QtQuickApp, use Qt plugin to bundle additional resources, and build AppImage, all in one single command
-
-./linuxdeploy-x86_64.AppImage --appdir AppDir --plugin qt --output appimage
+./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
 
 
 # move built AppImage back into original CWD
-
-mv QtQuickApp*.AppImage "$OLD_CWD"
+mv netcreator*.AppImage "$OLD_CWD"
